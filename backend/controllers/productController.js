2634ded2fb1c1857793c702acd7dea130bbbfc34
Brendan Cariota <brendan@cariota.com>
2021-01-12 04:03:06 -0600

    Added top rated product section on the homepage

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -150,6 +150,15 @@ const createProductReview = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Get Top Rated Products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({}).sort({ rating: -1 }).limit(3) // Sort will sort by rating in acsending order, limit will get only the first three
+
+    res.json(products)
+})
+
 export {
     getProducts,
     getProductById,
@@ -157,4 +166,5 @@ export {
     createProduct,
     updateProduct,
     createProductReview,
+    getTopProducts,
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 // REDUCERS
-import { productListReducer, productDetailsReducer, productDeleteReducer, productCreateReducer, productUpdateReducer, productReviewCreateReducer } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer, productDeleteReducer, productCreateReducer, productUpdateReducer, productReviewCreateReducer, productTopRatedReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer, userUpdateReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer, orderListReducer, orderDeliverReducer } from './reducers/orderReducers'
@@ -15,6 +15,7 @@ const reducer = combineReducers({
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
     productReviewCreate: productReviewCreateReducer,
+    productTopRated: productTopRatedReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
